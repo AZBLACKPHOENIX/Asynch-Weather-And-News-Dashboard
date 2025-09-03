43e969ac3d53389987060df1f7b46a429c5df236
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-09-03 20:10:52 +0200

    Update README

---
+++ testfile.ts
@@ -0,0 +1,10 @@
+import { CONFIG } from './src/config';
+
+
+console.log('CONFIG object:', CONFIG);
+
+
+console.log('Weather API URL:', CONFIG.WEATHER_API);
+console.log('News API URL:', CONFIG.NEWS_API);
+
+console.log('Number of news posts to fetch:', CONFIG.NEWS_LIMIT);
