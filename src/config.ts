3f96cbd9fe0389f0e1c8d08df2eaf4841bc0e086
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-09-03 18:04:18 +0200

    API file

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    WEATHER_API: 'https://api.open-meteo.com/v1/forecast',
+    NEWS_API: 'https://dummyjson.com/posts',
+    DEFAULT_COORDS: { latitude: -26.2041, longitude: 28.0473 }, // ✅ corrected
+    NEWS_LIMIT: 5
+};
