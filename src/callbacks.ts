f63a10ba1f633abfca7c772005ee377fd6dd8979
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-09-04 14:09:08 +0200

    USES CALLBACKS TO FETCH DARA

--- src/callbacks.ts
+++ src/callbacks.ts
@@ -20,3 +20,49 @@ function getWithCallback(url:string, callback:(err:Error | null, data?:string)=>
         console.error(err)
     })
 }
+
+export function fetchWeatherCallback(latitude:number, longitude:number, callback:(err:Error | null, data?:any)=>void){
+    const url = `${CONFIG.WEATHER_API}?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+    getWithCallback(url, (err, data)=>{
+        if (err){
+            return callback(err)
+         }
+       try{
+            const parsed = JSON.parse(data!)
+            callback(null, parsed)
+       }catch(parsedErr){
+            callback(parsedErr as Error)
+        }
+    })
+}
+
+export function fetchNewsCallback(limit: number, callback: (err: Error | null, data?: any) => void) {
+    const url = `${CONFIG.NEWS_API}?limit=${limit}`;
+  
+    getWithCallback(url, (err, data) => {
+      if (err) return callback(err);
+  
+      try {
+        const parsed = JSON.parse(data!);
+        callback(null, parsed);
+      } catch (parseErr) {
+        callback(parseErr as Error);
+      }
+    });
+  }
+
+  fetchWeatherCallback(CONFIG.DEFAULT_COORDS.latitude, CONFIG.DEFAULT_COORDS.longitude, (err, weather) => {
+    if (err) {
+      console.error("Weather error:", err);
+    } else {
+      console.log("Weather (Callback):", weather);
+  
+      fetchNewsCallback(CONFIG.NEWS_LIMIT, (err, news) => {
+        if (err) {
+          console.error("News error:", err);
+        } else {
+          console.log("News (Callback):", news);
+        }
+      });
+    }
+  });
